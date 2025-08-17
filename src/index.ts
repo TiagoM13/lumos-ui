41ea69adb8a4a823c9c9d1f2401f8de253d4dc62
TiagoM13 <thyagomotha2000@gmail.com>
2025-08-16 22:26:24 -0300

    ✨ feat: initial config project

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { LButton } from "./components/button/button";
+export { LInput } from "./components/input/input";
+export { LCard } from "./components/card/card";
+export { LModal } from "./components/modal/modal";
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+    coverage: {
+      reporter: ["text", "lcov"],
+      all: true,
+      include: ["src/components/**/*.tsx"],
+      exclude: ["node_modules", "dist"],
+    },
+  },
+});
